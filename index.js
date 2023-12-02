3a08fe423344aa6712cdb96dd30bd6feb8cff219
Zohir Bala <zohirbala@Zohirs-Air.broadband>
2023-12-02 20:26:11 +0000

    final upload

--- index.js
+++ index.js
@@ -17,44 +17,35 @@ function swap(block){
 
 document.addEventListener("keydown", function(event) {
     const blocks = document.querySelectorAll('.block');
-
-    function highlightFocusedBlock() {
-        blocks.forEach(block =>{
-            if(document.activeElement === block){
-                block.style.color = "purple";
-                console.log("active");
-            } else{
-                block.style.color = "black";
-                console.log('not active');
-            }
-        })
-    }
+      
     
     if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
         event.preventDefault(); 
         let currentIndex = -1;
 
+            blocks.forEach((block, index) => {
+                    if (document.activeElement === block) {
+                        currentIndex = index;  
+                        console.log(`delayed index ${currentIndex}`);
+                    }                
 
-        blocks.forEach((block, index) => {
-
-            
-            if (document.activeElement === block) {
-                currentIndex = index;                
-            }
-            
         });
         
         if (event.key === 'ArrowDown') {
+            
             currentIndex = (currentIndex + 1) % blocks.length;
+            console.log(currentIndex);
         } else if (event.key === 'ArrowUp') {
             currentIndex = (currentIndex - 1 + blocks.length) % blocks.length;
+            console.log(currentIndex);
         }
 
-        blocks[currentIndex].focus();{}
+        blocks[currentIndex].focus();
     } else if (event.key === 'Enter' || event.key === ' ') {
         swap(document.activeElement);
     }
 });
+
 
 
 
