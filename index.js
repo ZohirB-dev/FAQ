a90a648a7d148b9a414f71eda517607324155738
Zohir Bala <zohirbala@Zohirs-Air.broadband>
2023-12-02 19:09:11 +0000

    Smooth hide/show transition

--- index.js
+++ index.js
@@ -1,21 +1,60 @@
 
-
-function swap(question, answer){
+function swap(block){
     
-    const questions = document.getElementById(question);
-    const answers = document.getElementById(answer);
-
-    if (answers.style.display === "none"){
-        answers.style.display = "block";
-        questions.src = "assets/images/icon-minus.svg";
-    } else{
-        answers.style.display = "none";
+    const questions = block.querySelector('.image-class');
+    const answers = block.querySelector('.answer');
+    
+    if (answers.style.maxHeight){
+        answers.style.maxHeight = null;
+        answers.style.margin = null;
         questions.src = "assets/images/icon-plus.svg"
+    } else{
+        answers.style.maxHeight = answers.scrollHeight + "px";
+        answers.style.margin = '16px 0px';
+        questions.src = "assets/images/icon-minus.svg";
     }
 }
 
+document.addEventListener("keydown", function(event) {
+    const blocks = document.querySelectorAll('.block');
+
+    function highlightFocusedBlock() {
+        blocks.forEach(block =>{
+            if(document.activeElement === block){
+                block.style.color = "purple";
+                console.log("active");
+            } else{
+                block.style.color = "black";
+                console.log('not active');
+            }
+        })
+    }
+    
+    if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
+        event.preventDefault(); 
+        let currentIndex = -1;
 
 
-// principle learnt is that to be specific but effecient, provide the html with as much information as possible or get as much information as possible from the user if it requires user input.
+        blocks.forEach((block, index) => {
 
-// Then use the information provided to make a simple function
+            
+            if (document.activeElement === block) {
+                currentIndex = index;                
+            }
+            
+        });
+        
+        if (event.key === 'ArrowDown') {
+            currentIndex = (currentIndex + 1) % blocks.length;
+        } else if (event.key === 'ArrowUp') {
+            currentIndex = (currentIndex - 1 + blocks.length) % blocks.length;
+        }
+
+        blocks[currentIndex].focus();{}
+    } else if (event.key === 'Enter' || event.key === ' ') {
+        swap(document.activeElement);
+    }
+});
+
+
+
